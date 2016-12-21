8836150d8a7bde131211e23b726012edf29ffb9e
alemela <alessiom92@gmail.com>
2016-12-21 17:39:23 +0100

    improved all the stack that now works with real data!

--- etl.js
+++ etl.js
@@ -3,7 +3,9 @@ var MongoClient = require('mongodb').MongoClient;
 var config = require('./config.js');
 
 //XXX if loop, can lasts forever
-var findCat = function (current, parent) {
+var catCounter = 0;
+var findCat = function (current, parent, callback) {
+    catCounter++;
     console.log("[ADD] category " + current +  " to mongo");
     categories.update(
         { page_title: current },
@@ -16,27 +18,73 @@ var findCat = function (current, parent) {
             }
             console.log("[DONE] category " + current + " in mongo");
             //console.log(result)
-    })
 
+            //NEXT!
 
-    var findCatChildren = "SELECT page_title \
-                           FROM categorylinks, page \
-                           WHERE cl_to = '" + current + "' \
-                           AND page_id = cl_from \
-                           AND page_namespace = 14"
+            var findCatChildren = "SELECT page_title \
+                                   FROM categorylinks, page \
+                                   WHERE cl_to = '" + current + "' \
+                                   AND page_id = cl_from \
+                                   AND page_namespace = 14"
 
-    console.log("[ASK] category query");
-    config.connectionToWMF.query(findCatChildren, function(err, rows) {
+            console.log("[ASK] subcategories for " + current);
+            config.connectionToWMF.query(findCatChildren, function(err, rows) {
+                if (err) {
+                    console.log(err);
+                    process.exit(1);
+                }
+                console.log("[GOT] subcategories for " + current);
+                //console.dir(rows);
+
+                if (rows.length == 0) {
+                    catCounter--;
+                }
+
+                for (var r = 0; r < rows.length; r++) {
+                    findCat(rows[r].page_title, current, callback);
+                }
+
+                if (catCounter == 1) {
+                    callback();
+                }
+            });
+        })
+}
+
+var filesInCat = function (current, callback) {
+    var query = "SELECT img_name, img_user_text, img_timestamp, img_size \
+                 FROM categorylinks, page, image \
+                 WHERE cl_to = '" + current + "' \
+                 AND page_id = cl_from \
+                 AND page_namespace = 6 \
+                 AND img_name = page_title \
+                 LIMIT 100"; //XXX remove limit
+
+    console.log("[ASK] files for " + current);
+    config.connectionToWMF.query(query, function(err, rows) {
         if (err) {
             console.log(err);
             process.exit(1);
         }
-        console.log("[GOT] category results");
-        //console.dir(rows);
+        console.log("[GOT] files for " + current);
 
-        for (var r = 0; r < rows.length; r++) {
-            findCat(rows[r].page_title, current);
+        console.log("[ADD] files for " + current +  " to mongo");
+        var fileSaved = 1;
+        for (var f = 0; f < rows.length; f++) {
+            files.update(
+                { img_name: rows[f].img_name },
+                { $set: rows[f] },
+                { upsert: true },
+                function(err, result) {
+                    fileSaved++;
+                    if (fileSaved == rows.length) {
+                        console.log("[DONE] files for " + current +  " in mongo");
+                        callback();
+                    }
+                }
+            );
         }
+
     });
 }
 
@@ -46,6 +94,11 @@ MongoClient.connect(config.mongoURL, function(err, db) {
     categories = db.collection('category');
     files = db.collection('file');
 
-    //findCat(config.STARTING_CAT);
-    findCat("San_Vitale_(Ravenna)", null);
+    console.log("Starting collecting subcategories from " + config.STARTING_CAT)
+    findCat(config.STARTING_CAT, null, function () {
+        console.log("Downloading file info from " + config.STARTING_CAT)
+        filesInCat(config.STARTING_CAT, function () {
+            process.exit(0);
+        });
+    });
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var apicache = require('apicache').options({ debug: false }).middleware;
 var morgan = require('morgan');
 var MongoClient = require('mongodb').MongoClient;
+var config = require('./config.js');
 
 
 var app = express();
@@ -16,40 +17,34 @@ function auth (key, fn) {
     fn(null, null)
 }
 
+MongoClient.connect(config.mongoURL, function(err, db) {
+    console.log("Connected correctly to server");
 
-var url = 'mongodb://localhost:27017/cassandra';
-MongoClient.connect(url, function(err, db) {
-  console.log("Connected correctly to server");
-
-
-  app.get('/api/:id', apicache("1 day"), function (request, response) {
-    launchMongoQuery(request, response, request.params.id, "application/json");
-  });
-
-  app.get('*', function(req, res){
-    //res.send('what???', 404);
-    res.send(400);
-  });
-
-
-  var launchMongoQuery = function(req, res, id, type) {
-
-    db.collection('documents', function(err, collection) {
-      if (!err) {
-        collection.find({}).toArray(function(err, docs) {
-          res.send(docs);
-        });
-      } else {
-        console.log(err)
-      }
+    app.get('/api/:id', apicache("1 day"), function (request, response) {
+      launchMongoQuery(request, response, request.params.id, "application/json");
     });
-  }
+
+    app.get('*', function(req, res){
+      res.send(400);
+    });
 
 
-  var server = app.listen(80, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Server listening at http://%s:%s', host, port);
-  });
+    var launchMongoQuery = function(req, res, id, type) {
+        db.collection('file', function(err, collection) {
+            if (!err) {
+                collection.find({}).toArray(function(err, docs) {
+                    res.send(docs);
+                });
+            } else {
+                console.log(err)
+            }
+        });
+    }
 
+
+    var server = app.listen(80, function() {
+        var host = server.address().address;
+        var port = server.address().port;
+        console.log('Server listening at http://%s:%s', host, port);
+    });
 });
