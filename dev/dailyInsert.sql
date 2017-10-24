5d7a7a2adb38f325c39586818aa11c7c28c624ae
Dario Salza <dsalza@hotmail.it>
2017-10-24 14:05:18 +0200

    Aggiunta pk su accessi, e creata funzione di inserimento accesso

---
+++ dev/dailyInsert.sql
@@ -0,0 +1,13 @@
+﻿create or replace function dailyInsert(text, date,integer, integer, integer)
+returns integer as $$
+declare id integer;
+begin
+select media_id into id from media_index where media_name=$1;
+if not found then
+insert into media_index(media_name) values ($1) returning media_id into id;
+end if;
+insert into media_accesses values(id,$2,$3,$4,$5);
+return id;
+end;
+$$language plpgsql;
+select * from dailyInsert('lallo','22/10/2017',1,2,3)
--- dev/db_init.sql
+++ dev/db_init.sql
@@ -1,2 +1,2 @@
 create table media_index (media_name varchar(255) primary key, media_id serial unique)
-create table media_accesses (media_id int references media_index(media_id), access_date date, accesses bigint, wm_accesses bigint, nwm_accesses bigint)
+create table media_accesses (media_id int references media_index(media_id), access_date date, accesses bigint, wm_accesses bigint, nwm_accesses bigint, primary key(media_id,access_date))
