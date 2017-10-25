97e43b298bcebfab110e161b35ebb0ec2be73a5d
Dario Salza <dsalza@hotmail.it>
2017-10-25 17:45:47 +0200

    Working!

--- dev/dailyInsert.sql
+++ dev/dailyInsert.sql
@@ -6,8 +6,12 @@ select media_id into id from media_index where media_name=$1;
 if not found then
 insert into media_index(media_name) values ($1) returning media_id into id;
 end if;
+begin
 insert into media_accesses values(id,$2,$3,$4,$5);
+EXCEPTION   
+    WHEN OTHERS THEN RETURN -1;
+end;
 return id;
 end;
 $$language plpgsql;
-select * from dailyInsert('lallo','22/10/2017',1,2,3)
+select * from dailyInsert('lallo','23/10/2017',1,2,3)
--- dev/run.py
+++ dev/run.py
@@ -17,76 +17,137 @@ import sys
 import getopt
 import bz2
 import urllib
-import multiprocessing
+import threading
+import Queue
 import psycopg2 #postgres driver
-cur=None
+date=""
+conn=None
+watched=None
+BUF_SIZE = 200
+NTHREADS=5
+q = Queue.Queue(BUF_SIZE)
+isOver=False
+counter=0
+counterLock=threading.Lock()
+
+class ConsumerThread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, verbose=None):
+        super(ConsumerThread,self).__init__()
+        self.target = target
+        self.name = name
+        self.cur=conn.cursor()
+        return
+
+    def end(self):
+        self.cur.close()
+        #print "Died "+self.name
+        return
+
+    def calculate(self, arg):
+        # print arg
+        arr = arg.split("\t")
+        keysX = arr[0].split("/");
+        key = keysX[len(keysX) - 1]
+        # print key
+        if key in watched:
+            global counter
+            counterLock.acquire()
+            counter = counter + 1
+            counterLock.release()
+            print self.name+" Found " + key
+            query = "select * from dailyinsert('" + key.replace("'","''") + "','" + date + "'," + arr[2]+","+arr[22]+","+arr[23]+")"
+            #print query
+            self.cur.execute(query)
+
+    def run(self):
+        while not (isOver and q.empty()):
+            #print "Waiting "+self.name
+            item = q.get()
+            if(item=="DIE"):
+                break;
+            #print "Got "+item
+            self.calculate(item)
+            #print "Done "+self.name
+            q.task_done()
+        self.end()
+        #print self.name+"Really died"
+        return
 
 def reporter(first,second,third):
     if first%1000==0:
         print "Progress in the download: "+str(first*second*100/third)+"%"
 
-def calculate(arg,watched):
-    #print arg
-    arr=arg.split("\t")
-    keys=arr[0].split(".");
-    if len(keys)==2:
-        keys=keys[0]
-    elif len(keys)==3:
-        keys=keys[0]+"."+keys[1]
-    else:
-        return
-    keysX = keys.split("/");
-    key=keysX[len(keysX)-1]
-    if key in watched:
-        print key
-        print arr[2]
-        print arr[22]
-        print arr[23]
-
-
 def process(date,watchedfilename):
     day, month, year=date.split("/")
-    watchedfiles=set(line.strip() for line in open(watchedfilename))
+    global watched
+    watched=set(line.strip() for line in open(watchedfilename))
     baseurl="https://dumps.wikimedia.org/other/mediacounts/daily/"
     finalurl=baseurl+year+"/"+"mediacounts."+year+"-"+month+"-"+day+".v00.tsv.bz2"
     print ("Retrieving "+finalurl+"...")
-    '''filename,headers=urllib.urlretrieve(finalurl,'temp/temp.tsv.bz2',reporter)'''
-    filename="temp/temp.tsv.bz2"
+    filename,headers=urllib.urlretrieve(finalurl,'temp/temp.tsv.bz2',reporter)
+    #filename="temp/temp.tsv.bz2"
     print "Download completed."
     print filename
     source_file = bz2.BZ2File(filename, "r")
-    #pool = multiprocessing.Pool(processes=4)
-    #pool.map(calculate, [line for line in source_file])
+    print "Loading visualizations... this may take several minutes"
+    #crea thread
+    threads=[]
+    i=0
+    while i<NTHREADS:
+        c=ConsumerThread(name=str(i))
+        threads.append(c)
+        c.start()
+        i=i+1
     for line in source_file:
-        calculate(line,watchedfiles)
+        counterLock.acquire()
+        doBreak=False
+        #print counter
+        #print len(watched)
+        if counter==len(watched):
+            doBreak=True
+            #print "Killing in the name of!"
+        counterLock.release()
+        if doBreak:
+            break
+        #print "Put "+line
+        q.put(line)
+    i=0
+    global isOver
+    isOver=True
+    q.join()
+    i=0
+    while i<NTHREADS:
+        q.put("DIE") #sporchissimo, ma sembra non esserci altro modo
+        i=i+1
+    i=0
+    while i<NTHREADS:
+        threads[i].join()
+        i=i+1
 
-
+def init(argdate,argfname):
+    global cursors
+    global date
+    global conn
+    pgconnection = psycopg2.connect("dbname=CassandraTEST user=cassandra password=cassandra")
+    pgconnection.autocommit = True
+    conn=pgconnection
+    date = argdate
+    print "Script running with following parameters: "
+    print date
+    print argfname
+    process(date, argfname)
+    pgconnection.close()
+    print "Process ended"
 
 def main():
-    arg="01/01/2017"
-    global cur
-    pgconnection=psycopg2.connect("dbname=CassandraTEST user=cassandra password=cassandra")
-    pgconnection.autocommit=True
-    cur=pgconnection.cursor()
-    cur.execute("insert into media_index values ('micio',3) ")
-    cur.close()
-    pgconnection.close()
-    process(arg,watchedfilename="test.txt")
     # parse command line options
-    '''try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
-    except getopt.error, msg:
-        print msg
-        print "for help use --help"
+    print sys.argv
+    if len(sys.argv)==3:
+        init(sys.argv[1],sys.argv[2]);
+    else:
+        print "Not enough arguments. See the app documentation"
         sys.exit(2)
-    # process options
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print __doc__
-            sys.exit(0)
-    # process arguments
-    for arg in args:
-        process(arg) # process() is defined elsewhere'''
 
 if __name__ == "__main__":
     main()
