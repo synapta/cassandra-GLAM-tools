dac7f5e11bc67cd31e3ab09c2a8d7cdfee247c7c
Dario Salza <dsalza@hotmail.it>
2017-10-31 17:50:19 +0100

    bugfixes on the sql part

--- dev_categoryUpdate/maintenance.sql
+++ dev_categoryUpdate/maintenance.sql
@@ -3,7 +3,6 @@ create or replace function doMaintenance()
 returns void as $$
 begin
 insert into dailyImageUsage(img_name, count_date, count) select gil_to, CURRENT_DATE, count(*) from usages where last_seen=current_date group by gil_to;
-update images set is_alive=false where last_seen<(current_date-5);
-exception when others then return;
+update usages set is_alive=false where last_seen<(current_date-5);
 end;
 $$language plpgsql;
--- dev_dailyUpdate/dailyInsert.sql
+++ dev_dailyUpdate/dailyInsert.sql
@@ -3,7 +3,7 @@ returns integer as $$
 declare id integer;
 begin
 select media_id into id from images where img_name=$1;
-insert into media_accesses values(id,$2,$3,$4,$5);
+insert into visualizations values(id,$2,$3,$4,$5);
 return id;
 EXCEPTION   
     WHEN OTHERS THEN RETURN -1;
