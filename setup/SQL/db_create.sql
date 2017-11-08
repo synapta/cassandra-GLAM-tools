c465c9225d445c94b140c75a9087fe8dce47cc2e
Dario Salza <dsalza@hotmail.it>
2017-11-08 19:23:19 +0100

    Other fixes

--- setup/SQL/db_create.sql
+++ setup/SQL/db_create.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE "CassandraTEST"
+CREATE DATABASE "Cassandra"
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
--- setup/config.js
+++ setup/config.js
@@ -2,7 +2,7 @@ var {Pool, Client} = require('pg');
   var DB = new Client({
         user: 'Cassandra',
         host: '127.0.0.1',
-        database: 'CassandraTEST',
+        database: 'Cassandra',
         password: 'Cassandra',
         port: 5432
       });
