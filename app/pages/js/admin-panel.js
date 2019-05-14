e859c89da8aa94c1caae064b20296f4ab0b2c4a7
francescocretti <francesco.cretti@gmail.com>
2019-05-14 12:20:32 +0200

    new admin panel

--- app/pages/js/admin-panel.js
+++ app/pages/js/admin-panel.js
@@ -8,6 +8,11 @@ $(function() {
   // Get data
   $.getJSON('/api/admin/glams', function(items) {
     if (items.length > 0) {
+      let running = 0;
+      let paused = 0;
+      let failed = 0;
+      let pending = 0;
+      console.log(items.length);
       $.get('/views/templates/glam-preview.tpl', function(tpl) {
         var template = Handlebars.compile(tpl);
         items.forEach(function(el, idx) {
@@ -23,33 +28,51 @@ $(function() {
           obj.status = el.status;
           switch (obj.status) {
             case "running":
+              running++;
+              obj.command = "pause";
+              obj.paused = false;
+              break;
             case "pending":
+              pending++;
               obj.command = "pause";
               obj.paused = false;
               break;
             case "paused":
+              paused++;
               obj.command = "restart";
               obj.paused = true;
               break;
             case "failed":
+              failed++;
               obj.command = "retry";
               obj.paused = true;
               break;
           }
-          if (isEven(idx)) {
-            $('#glams-list-left').append(template(obj));
-          } else {
-            $('#glams-list-right').append(template(obj));
-          }
+          // if (isEven(idx)) {
+          //   $('#glams-list-left').append(template(obj));
+          // } else {
+          //   $('#glams-list-right').append(template(obj));
+          // }
+          $('#glams-list-unique').append(template(obj));
         });
-        // on hover
-        $('.glam-block').mouseenter( function() {
-          $(this).find('.glam-controls-overlay').fadeIn(200);
-          $(this).find('.glam-controls').fadeIn(200);
-        }).mouseleave( function() {
-          $(this).find('.glam-controls-overlay').fadeOut(100);
-          $(this).find('.glam-controls').fadeOut(100);
-        });
+        // Display counts
+        $('#total-glams').html(items.length);
+        $('#running-glams').html(running);
+        $('#pending-glams').html(pending);
+        $('#paused-glams').html(paused);
+        $('#failed-glams').html(failed);
+        if (is_touch_device()) {
+          console.log('touch screen');
+          // show always
+          $('.glam-controls').fadeIn();
+        } else {
+          // show on hover
+          $('.glam-block').mouseenter( function() {
+            $(this).find('.glam-controls').fadeIn(200);
+          }).mouseleave( function() {
+            $(this).find('.glam-controls').fadeOut(100);
+          });
+        }
         // on click pause/unpause
         $('.glam-block > .glam-controls.command').click(function() {
           let pause = !$(this).data('glampaused');
@@ -73,8 +96,53 @@ $(function() {
       $('#glams-list').html('<div class="w-100 text-center my-5"><h1>No available GLAMs</h1></div>');
     }
   });
+  // filter functions
+  $('.glam-filter').click(function() {
+    let id = this.id.replace('-glams', '');
+    filterGlams(id);
+  });
 });
 
 function isEven(number) {
   return number % 2 === 0;
+}
+
+function filterGlams(id) {
+  let $btn = $('#' + id + '-btn');
+  console.log($btn);
+  $('.filt-btn').removeClass('active-btn');
+  switch (id) {
+    case 'total':
+      $('.glam-block').fadeIn();
+      break;
+    case 'running':
+      $('.glam-block.pending').fadeOut(500, function() {
+        $('.glam-block.running').fadeIn(300);
+      });
+      $('.glam-block.paused').fadeOut(400);
+      $('.glam-block.failed').fadeOut(400);
+      break;
+    case 'pending':
+      $('.glam-block.running').fadeOut(500, function() {
+        $('.glam-block.pending').fadeIn(300);
+      });
+      $('.glam-block.paused').fadeOut(400);
+      $('.glam-block.failed').fadeOut(400);
+      break;
+    case 'paused':
+      $('.glam-block.running').fadeOut(500, function() {
+        $('.glam-block.paused').fadeIn(300);
+      });
+      $('.glam-block.pending').fadeOut(400);
+      $('.glam-block.failed').fadeOut(400);
+      break;
+    case 'failed':
+      $('.glam-block.running').fadeOut(500, function() {
+        $('.glam-block.failed').fadeIn(300);
+      });
+      $('.glam-block.pending').fadeOut(400);
+      $('.glam-block.paused').fadeOut(400);
+      break;
+  }
+  $btn.addClass('active-btn');
 }
--- app/pages/views/templates/glam-preview.tpl
+++ app/pages/views/templates/glam-preview.tpl
@@ -1,16 +1,18 @@
-<!-- <div class="col-12 col-md-6"> -->
-  <div class="glam-block {{status}}">
-    <div class="info">
+<div class="glam-block {{status}}">
+  <div class="info">
+    <div class="glam-controls-container">
       <h3 class="status">{{status}}</h3>
-      <h2>{{glamFullName}} ({{glamID}})</h2>
-      <h2><span class="bold-span">CAT:</span> <i>{{glamCategory}}</i></h2>
-      {{#if lastrun}} <h2> <span class="bold-span">LAST RUN:</span> {{lastrun}}</h2>{{/if}}
+      <a class="glam-controls edit" href="/admin/edit-glam/{{glamID}}">Edit</a>
+      <spans class="glam-controls command {{command}}" data-glamID={{glamID}} data-glamPaused={{paused}}>{{command}}</span>
     </div>
-    <div class="img">
-      <img src="{{image_url}}" alt="{{glamFullName}}">
-    </div>
-    <div class="glam-controls-overlay"></div>
-    <a class="glam-controls edit" href="/admin/edit-glam/{{glamID}}">EDIT</a>
-    <spans class="glam-controls command {{command}}"  data-glamID={{glamID}} data-glamPaused={{paused}}>{{command}}</span>
+    <h2>{{glamFullName}} ({{glamID}})</h2>
+    <h2><span class="bold-span">CAT:</span> <i>{{glamCategory}}</i></h2>
+    {{#if lastrun}}
+      <h2> <span class="bold-span">LAST RUN:</span> {{lastrun}}</h2>
+    {{/if}}
   </div>
-<!-- </div> -->
+  <div class="img">
+    <img src="{{image_url}}" alt="{{glamFullName}}">
+  </div>
+  <!-- <div class="glam-controls-overlay"></div> -->
+</div>
