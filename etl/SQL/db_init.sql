15625a4bed86fc8c93f58c5adeb45830dd8f8488
Diego Monti <diegmonti@gmail.com>
2019-09-03 15:03:20 +0200

    Add position to annotation

--- etl/SQL/db_init.sql
+++ etl/SQL/db_init.sql
@@ -1,7 +1,7 @@
 CREATE table IF NOT EXISTS categories (page_title varchar(255) primary key, cat_subcats int, cat_files int, cl_to varchar(255)[], cat_level int[]);
 CREATE table IF NOT EXISTS images (img_name varchar(255) primary key, img_user_text varchar(255), img_timestamp timestamp without time zone,img_size bigint,cl_to varchar(255)[],media_id serial unique,is_alive boolean);
 CREATE table IF NOT EXISTS visualizations (media_id int references images(media_id), access_date date, accesses bigint, wm_accesses bigint, nwm_accesses bigint, primary key(media_id,access_date));
-CREATE table IF NOT EXISTS annotations (annotation_date date primary key, annotation_value varchar(255));
+CREATE table IF NOT EXISTS annotations (annotation_date date primary key, annotation_value varchar(255), annotation_position varchar(255));
 CREATE table IF NOT EXISTS usages (gil_wiki varchar(20),gil_page_title varchar(255),gil_to varchar(255), first_seen date,last_seen date, is_alive boolean, primary key(gil_to,gil_page_title,first_seen,gil_wiki));
 CREATE INDEX IF NOT EXISTS ad_GBy on visualizations(access_date);
 create materialized view if not exists visualizations_sum as select sum(accesses) as accesses_sum, access_date from visualizations group by access_date;
