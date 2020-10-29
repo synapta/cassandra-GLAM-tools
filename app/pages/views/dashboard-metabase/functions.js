f880fde8c705ae3784f34da34bb36f5157f25be4
loredanaMaran <maranloredana@gmail.com>
2020-10-29 15:02:05 +0100

    Aggiunto dashboard metabase

---
+++ app/pages/views/dashboard-metabase/functions.js
@@ -0,0 +1,16 @@
+const glam = window.location.href.toString().split('/')[3];
+let SUBCATEGORY;
+
+function setCategoryCb(category) {
+	SUBCATEGORY = category;
+}
+
+$(document).ready(function() {
+	setCategory(setCategoryCb);
+	let iframeUrl = "";
+	$.getJSON('/api/metabase', function (res) {
+		const iframe = document.getElementById('dashboard-metabase');
+		console.log(res);
+		iframe.src = res.iframeUrl;
+	});
+});
