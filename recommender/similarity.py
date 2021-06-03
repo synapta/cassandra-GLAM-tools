bfd7704bece6ad171d3efa1bacb7c2c90dc9b910
Diego Monti <diegmonti@gmail.com>
2021-06-03 15:00:37 +0200

    Experiment, now obsolete code

--- recommender/similarity.py
+++ recommender/similarity.py
@@ -1,18 +1,12 @@
 import json
 import logging
-import pickle
 import sys
 from datetime import date, timedelta
 
-import langid
 import mwclient
-import mwparserfromhell
 import psycopg2
 import requests
 from psycopg2.errors import UniqueViolation
-
-from gensim import corpora, models, similarities
-from tokenizer import tokenize
 
 if len(sys.argv) == 1:
     print('Missing database')
@@ -51,68 +45,6 @@ images = cur.fetchall()
 image_counter = 1
 
 site = mwclient.Site('commons.wikimedia.org')
-
-
-def get_description(image):
-    wikipage = site.images[image]
-    text = wikipage.text()
-    wikicode = mwparserfromhell.parse(text)
-    templates = wikicode.filter_templates(matches="{{Information")
-
-    if len(templates) > 0:
-        description = templates[0].get("Description").value
-        wikicode = mwparserfromhell.parse(description)
-        en_templates = wikicode.filter_templates(matches="{{en")
-
-        if len(en_templates) > 0:
-            description = en_templates[0].get("1").value
-
-        description = description.strip_code(keep_template_params=True)
-        description = " ".join(description.split())
-        language = langid.classify(description)
-
-        if len(description) > 0:
-            return language[0], description
-
-    return None, None
-
-
-def load_model(name):
-    with open(name + '.pkl', 'rb') as fp:
-        id2entity = pickle.load(fp)
-    dictionary = corpora.Dictionary.load(name + '.dict')
-    model = models.TfidfModel.load(name + '.tfidf')
-    index = similarities.Similarity.load(name + '.index')
-    return id2entity, dictionary, model, index
-
-
-def compute_similarity(metamodel, description):
-    id2entity, dictionary, model, index = metamodel
-
-    tokens = tokenize(description)
-    vec_bow = dictionary.doc2bow(tokens)
-    vec_model = model[vec_bow]
-    sims = index[vec_model]
-    sims = sorted(enumerate(sims), key=lambda item: -item[1])
-
-    stop_at = 0
-
-    for i, sim in enumerate(sims):
-        if sim[1] < 0.4 or i > 10:
-            stop_at = i
-            break
-
-    sims = sims[:stop_at]
-
-    entities = []
-    scores = {}
-
-    for entity in sims:
-        id_entity = id2entity[entity[0]]
-        entities.append(id_entity)
-        scores[id_entity] = entity[1]
-
-    return entities, scores
 
 
 def compute_category(image):
@@ -174,9 +106,6 @@ def process_entities(image, entities, scores):
             continue
 
 
-# metamodel_en = load_model('en/model')
-# metamodel_de = load_model('de/model')
-
 for image in images:
     logging.info('Processing image %s of %s: %s', image_counter, len(images), image[0])
     image_counter += 1
@@ -187,16 +116,6 @@ for image in images:
 
         entities = compute_category(image[0])
         process_entities(image[0], entities, None)
-
-        # language, description = get_description(image[0])
-
-        # if description is not None:
-        #     if language == 'de':
-        #         entities, scores = compute_similarity(metamodel_de, image[0] + ' ' + description)
-        #     else:
-        #         entities, scores = compute_similarity(metamodel_en, image[0] + ' ' + description)
-
-        #     process_entities(image[0], entities, scores)
 
     except ValueError:
         continue
